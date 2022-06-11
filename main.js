e8a466c0ea4fa450ebf9bb9f885a32ea25c45f2b
Chinmay-tech <79971968+Chinmay-tech@users.noreply.github.com>
2022-06-11 15:44:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/C5NW5ty7a/model.json',modelReady);    
+}
+
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
